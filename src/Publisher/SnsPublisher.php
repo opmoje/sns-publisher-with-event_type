2dc17ee8a711b8147d50249c9961a656a9a4604b
Vladislav <raytracer@yandex.ru>
2020-05-04 16:45:59 +0500

    Custom publisher ready

---
+++ src/Publisher/SnsPublisher.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace Opmoje\Publisher;
+
+
+use Aws\Sns\SnsClient;
+use SimpleBus\Asynchronous\Publisher\Publisher;
+use SimpleBus\Serialization\Envelope\Serializer\StandardMessageInEnvelopeSerializer;
+
+class SnsPublisher implements Publisher
+{
+    /** @var SnsClient */
+    private $client;
+
+    /** @var string */
+    private $topic;
+
+    /** @var StandardMessageInEnvelopeSerializer */
+    private $serializer;
+
+    public function __construct(SnsClient $client, StandardMessageInEnvelopeSerializer $serializer, string $topic)
+    {
+        $this->client = $client;
+        $this->topic = $topic;
+        $this->serializer = $serializer;
+    }
+
+    public function publish($message)
+    {
+        $this->client->publish(
+            [
+                'TopicArn' => $this->topic,
+                'Message' => $this->serializer->wrapAndSerialize($message),
+                'MessageAttributes' => [
+                    'event_type' => [
+                        'DataType' => 'String',
+                        'StringValue' => $message->messageName()
+                    ],
+                ],
+            ]
+        );
+    }
+}
